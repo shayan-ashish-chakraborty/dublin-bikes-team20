cd2681c0a1143d6c73f916746d74cde732bd792c
shayan-chakraborty1664 <shayanchakraborty1664@gmail.com>
2026-02-26 17:56:42 +0000

    Minor issue rectified

--- sprint1_webscrappers/jcdecaux_api/get-station.py
+++ sprint1_webscrappers/jcdecaux_api/get-station.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 if os.path.exists("var.env"):
     load_dotenv(dotenv_path="var.env")
 
-r = requests.get(os.getenv('STATION_URI'), params={"apiKey": os.getenv('JCDECAUX_API_KEY'), "contract": os.getenv('CITY')})
+r = requests.get(os.getenv('STATIONS_URI'), params={"apiKey": os.getenv('JCDECAUX_API_KEY'), "contract": os.getenv('CITY')})
 
 data = json.loads(r.text)
 print(json.dumps(data, indent=4))
