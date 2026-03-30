1aa59962e85feaed08619d736cd6fb215f583b08
Leah Unnikrishnan <leah.unnikrishnan@ucdconnect.ie>
2026-03-31 00:10:01 +0100

    Login/Register
    1. Created a new folder for blueprint of user authentication to group them- backend/routes/auth.py
    2. Imported auth blueprint file to __init__.py
    3. Functionality and frontend of Login/Register

--- backend/__init__.py
+++ backend/__init__.py
@@ -3,14 +3,19 @@ from flask import Flask
 from .config import Config
 from .routes.bikes import bikes_bp
 from .routes.weather import weather_bp
-
+from .routes.auth import auth_bp #imported the auth page 
+import os
 
 def create_app() -> Flask:
     app = Flask(__name__)
     app.config.from_object(Config())
 
+    # Set secret key for sessions and flash messages
+    app.secret_key = os.getenv("SECRET_KEY", "devsecret")  # replace in production
+
     app.register_blueprint(weather_bp, url_prefix="/api/weather")
     app.register_blueprint(bikes_bp, url_prefix="/api/bikes")
+    app.register_blueprint(auth_bp, url_prefix="/auth") #auth blueprint within create app function
 
     @app.get("/")
     def index():
